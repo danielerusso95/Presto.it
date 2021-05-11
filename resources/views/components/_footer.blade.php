27268a2c7c646d8bcf6dfe5fc767ae04effcdf6a
Grace <grazia.cassanod@gmail.com>
2021-05-11 15:46:37 +0200

    creazione template con scaffolding e building degli asset. DB collegato

---
+++ resources/views/components/_footer.blade.php
@@ -0,0 +1,7 @@
+<div class="container bg-dark shadow fixed-bottom text-light">
+    <div class="row">
+        <div class="col-12">
+            <h4>Footer</h4>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presto.it</title>
+    <link rel="stylesheet" href="/css/app.css">
+    {{$style ?? ''}}
+</head>
+<body>
+    
+    <x-_navbar />
+
+    {{$slot}}
+
+    <x-_footer />
+
+    <script src="/js/app.js"></script>
+
+    @stack('scripts')
+    
+</body>
+</html>
