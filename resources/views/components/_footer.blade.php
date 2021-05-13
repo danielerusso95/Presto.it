db891d45204900bc4bae18a46fcc76bd667d5b4f
Grace <grazia.cassanod@gmail.com>
2021-05-13 10:42:18 +0200

    footer sistemato fisso in fondo

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid bg-primary shadow fixed-bottom text-light py-3">
+<div class="container-fluid bg-primary shadow footer text-light py-3">
     <div class="row">
         <div class="col-12 text-center">
             <a href=""><i class="fab fa-facebook text-white mx-3"></i></a>
@@ -6,7 +6,6 @@
             <a href=""><i class="fab fa-linkedin text-white mx-3"></i></a>
             <a href=""><i class="fab fa-twitter text-white mx-3"></i></a>
             <a href=""><i class="fab fa-google text-white mx-3"></i></a>
-
         </div>
     </div>
 </div>
