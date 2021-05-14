884f59acf76e489b26b5d4bbded2e559dd715cba
Daniele Trizio <daniele.trizio91@outlook.it>
2021-05-14 14:53:34 +0200

    fix immagine in bin

--- resources/views/revisor/bin.blade.php
+++ resources/views/revisor/bin.blade.php
@@ -10,7 +10,7 @@
     @foreach($articles as $article)
                 <div class="col-12 col-md-4 mb-5 ">
                     <div class="card mx-auto" style="width: 18rem;">
-                        <img src="{{$images[0]}}" class="card-img-top" alt="image random">
+                        <img src="{{$images[0]}}200" class="card-img-top" alt="image random">
                         <div class="card-body">
                             <h5 class="card-title">Titolo: {{$article->title}}</h5>
                             <p class="card-subtitle">Prezzo: {{$article->price}} €</p>
