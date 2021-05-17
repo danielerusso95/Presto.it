19c9c8ff7e959724ef2ef017df4b3fc2fee5aa6c
daniele.russo <russo.daniele0@gmail.com>
2021-05-17 14:34:50 +0200

    fix caricamento categories in boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,10 +28,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $categories = Category::all();
-        View::share('categories',$categories);
-        Paginator::useBootstrap();
-        $articles_home = Article::orderByDesc('created_at')->paginate(5);
-        View::share('articles_home',$articles_home);
+        if (Schema::hasTable('categories')) {
+            $categories = Category::all();
+            View::share('categories',$categories);
+            Paginator::useBootstrap();
+            $articles_home = Article::orderByDesc('created_at')->paginate(5);
+            View::share('articles_home',$articles_home);
+        }
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
-require('./bootstrap');
+require("./bootstrap");
 
-require('./js_custom');
+require("./js_custom");
